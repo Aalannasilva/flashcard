59c024bec0c971a1f58620b3450020705374b1bd
Aalannasilva <163008137+Aalannasilva@users.noreply.github.com>
2024-10-07 11:33:49 -0300

    .

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'ingles',
+    'o que é phyton?',
+    'o phyton é uma linguagem de programação'
+)
+
+criaCartao(
+    'portugues',
+    'o que é um verbo?',
+    'o verbo é uma ação, processo ou estado'
+)
+
+criaCartao(
+    'sociologia',
+    'qual o objetivo da sociologia?',
+    'O objetivo da sociologia é estudar, entender e classificar as formações sociais'
+)
+
+criaCartao(
+    'geografia',
+    'qual o país com o clima mais frio?',
+    'a rússia é o país com o clima mais frio do mundo. Considerando a temperatura média anual, chega aproximadamente a -3,64 ºC.'
+)
+
+criaCartao(
+    'biologia',
+    'a doença de chagas é causada por bácterias, fungos ou parasitas?',
+    'a doença de chagas é causada por um parasita, principalmente pelo inseto chamado barbeiro'
+)
+
+criaCartao(
+    'história',
+    'quais foram os países que faziam parte da união soviética?',
+    'os países que faziam parte da união soviética eram: Armênia, Azerbaijão, Bielorússia (hoje Belarus), Estônia, Geórgia, Cazaquistão, Quirguistão, Letônia, Lituânia, Moldávia, Rússia, Tadjiquistão, Turcomenistão, Ucrânia e Uzbequistão.'
+)
